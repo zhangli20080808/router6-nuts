3265caa77b6b8fdf8a44a919fc0a816e2c460c60
zhangli20080808 <791492434@qq.com>
2022-12-30 18:35:25 +0800

    react-route6 init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path='person' element={<Person />}></Route>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
+}
+
+function Layout(params) {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Link to='/'> 首页</Link>
+      <Link to='/person'> 个人中心</Link>
+      <Outlet/>
+    </div>
+  );
+}
+
+function Home(params) {
+  return <div>home</div>;
+}
+function Person(params) {
+  return <div>person</div>;
 }
 
 export default App;
