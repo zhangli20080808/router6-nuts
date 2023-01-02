f2d8023fc3c6372ea0921da623dc65a18b9481c3
zhangli20080808 <791492434@qq.com>
2023-01-02 15:03:28 +0800

    feat：路由守卫

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link,
-//   Outlet,
-//   // NavLink,
-//   useParams,
-//   useNavigate,
-//   // Navigate
-//   // createBrowserRouter
-//   // createHashRouter
-// } from 'react-router-dom';
 import {
   BrowserRouter,
   Routes,
@@ -19,41 +6,66 @@ import {
   Outlet,
   useNavigate,
   useParams,
-} from './mini-react-router';
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+//   Link,
+//   Outlet,
+//   useNavigate,
+//   useParams,
+//   useLocation
+// } from './mini-react-router';
 import './App.css';
+import { AuthProvider, useAuth } from './mini-react-router/auth';
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            {/* Route 的 index 属性就是用来展示默认子路由的 */}
-            <Route path='/' element={<Home />}></Route>
-            <Route path='product' element={<Product />}>
-              <Route path=':id' element={<ProductDetail />}></Route>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              {/* Route 的 index 属性就是用来展示默认子路由的 */}
+              <Route path='/' element={<Home />}></Route>
+              <Route path='product' element={<Product />}>
+                <Route path=':id' element={<ProductDetail />}></Route>
+              </Route>
+              <Route
+                path='user'
+                element={
+                  <RequiredAuth>
+                    <User />
+                  </RequiredAuth>
+                }
+              />
+              <Route path='login' element={<Login />} />
+              <Route path='*' element={<NoMatch />}></Route>
             </Route>
-            {/* <Route path='*' element={<NoMatch />}></Route> */}
-          </Route>
-          {/* <Route path='product' element={<Product />} /> */}
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
 
-function Layout(params) {
+function Layout() {
   return (
     <div>
       <h1>Layout</h1>
       <Link to='/'> 首页</Link>
       <Link to='/product'> 商品中心</Link>
+      <Link to='/user'> 用户中心</Link>
+      <Link to='/login'> 登录</Link>
       <Outlet />
     </div>
   );
 }
 
-function Home(params) {
+function Home() {
   return (
     <div>
       home
@@ -63,7 +75,7 @@ function Home(params) {
     </div>
   );
 }
-function Product(params) {
+function Product() {
   return (
     <div>
       <h1>product</h1>
@@ -83,6 +95,63 @@ function ProductDetail() {
       {/* 命令跳转方式 */}
       <button onClick={() => navigate('/')}>go Home</button>
     </>
+  );
+}
+
+function RequiredAuth({ children }) {
+  const auth = useAuth();
+  const location = useLocation();
+  if (!auth.user) {
+    return <Navigate to={'/login'} state={{ from: location }} replace={true} />;
+  }
+  return children;
+}
+
+function User() {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  console.log(auth,'auth');
+  return (
+    <div>
+      <h1>User</h1>
+      <p>{auth.user?.username}</p>
+      <button
+        onClick={() => {
+          auth.signout(() => navigate("/login"));
+        }}
+      >
+        退出登录
+      </button>
+    </div>
+  );
+}
+
+function Login() {
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from.pathname || '/';
+
+  if (auth.user) {
+    return <Navigate to={from} />;
+  }
+
+  const submit = (e) => {
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get('username');
+    auth.signin({ username }, () => {
+      navigate(from, { replace: true });
+    });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={submit}>
+        <input type='text' name='username' />
+        <button type='submit'>login</button>
+      </form>
+    </div>
   );
 }
 
--- src/mini-react-router/Outlet.js
+++ src/mini-react-router/Outlet.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { useOutlet } from './hooks';
 export default function Outlet() {
   return useOutlet();
